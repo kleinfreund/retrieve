9182901000eda6c9ee20c08083b3a827802f0db2
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2025-03-31 21:50:00 +0200

    chore(pkg): update dist files

--- dist/retrieve.d.ts
+++ dist/retrieve.d.ts
@@ -1,3 +1,28 @@
+declare class RetrieveResponse {
+    /**
+     * Original `Request` object passed to `fetch`.
+     */
+    request: Request;
+    /**
+     * Original `Response` object returned by `fetch`.
+     */
+    response: Response;
+    /**
+     * Deserialized response body (if applicable).
+     *
+     * The following logic applies to deserialization:
+     *
+     * - Response content type starts with “application/json” or “application/problem+json”: the response body is parsed as JSON (using `Response.prototype.json`).
+     * - For everything else: the response body is parsed as text (using `Response.prototype.text`).
+     */
+    data: unknown;
+    constructor({ request, response, data }: {
+        request: Request;
+        response: Response;
+        data: unknown;
+    });
+}
+
 interface RetrieveConfig {
     /**
      * Request URL.
@@ -31,7 +56,7 @@ interface RetrieveConfig {
      *   - “plain/text” if `config.data` is a string
      *   - “application/json” if `config.data` is set and the request method isn't GET or HEAD
      *
-     *   Note that if `config.data` is set to a `FormData` object, an existing content type **will be removed**. Read the warning on [MDN: Using FormData Objects: Sending files using a FormData object](https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects#sending_files_using_a_formdata_object) for an explanation.
+     *   Note, that if `config.data` is set to a `FormData` object, an existing content type **will be removed**. Read the warning on [MDN: Using FormData Objects: Sending files using a FormData object](https://developer.mozilla.org/en-US/docs/Web/API/FormData/Using_FormData_Objects#sending_files_using_a_formdata_object) for an explanation.
      * - **Body**: If `config.data` is set, it will be used for `init.body`. See `config.data` description for more information. Otherwise, if `config.init.body` is set, it will be used for fetch's `init.body`.
      * - **Signal**: If `config.timeout` is set to a positive number, it will be used to create `init.signal` using `AbortSignal.timeout(config.timeout)`.
      */
@@ -42,23 +67,9 @@ interface RetrieveConfig {
      * If `config.data` is set:
      *
      * - … and the “content-type” header is “application/json”, `init.body` is set to the result of `JSON.stringify(config.data)`
-     * - … otherwise, `init.body` is set to `config.data`. It's your responsibility to make sure `config.data` can used on `init.body` (see [fetch() global function: parameters](https://developer.mozilla.org/en-US/docs/Web/API/fetch#parameters)).
+     * - … otherwise, `init.body` is set to `config.data`. It's your responsibility to make sure `config.data` can be used on `init.body` (see [fetch() global function: parameters](https://developer.mozilla.org/en-US/docs/Web/API/fetch#parameters)).
      */
     data?: any;
-    /**
-     * Message for request errors.
-     *
-     * If set, it overrides the underlying error's own message which will then be set on the request error's `cause` property.
-     *
-     * **Default**: `'Unknown request error'`
-     */
-    requestErrorMessage?: string;
-    /**
-     * Message for response errors.
-     *
-     * **Default**: `$statusCode $statusText` (e.g. `'404 Not Found'`)
-     */
-    responseErrorMessage?: string;
     /**
      * Request timeout in milliseconds.
      *
@@ -66,7 +77,16 @@ interface RetrieveConfig {
      */
     timeout?: number;
     /**
-     * Processed right before a request is sent (i.e. before calling `fetch`). Allows making changes to the parameters passed to `fetch` after they've been processed by `retrieve`.
+     * Run right before a request is sent (i.e. before calling `fetch`). Allows making changes to the parameters passed to `fetch` after they've been processed by `retrieve`. Also allows skipping the call to `fetch` entirely.
+     *
+     * Exceptions during the processing of a before request handler are not caught.
+     *
+     * A before request handler can have one of two results:
+     *
+     * - proceeding with the execution of `retrieve` like normal
+     * - altering the normal execution of `retrieve` to avoid making the call to `fetch` entirely (indicated by returning a `Response` object)
+     *
+     * When returning a `Response` object in a before request handler, the call to `fetch` is skipped entirely and the subsequent execution of `retrieve` uses the `Response` object for all further logic.
      *
      * **Example**:
      *
@@ -74,11 +94,8 @@ interface RetrieveConfig {
      * const config = {
      *   url: 'https://api.example.org',
      *   beforeRequestHandlers: [
-     *     (url, init) => {
-     *       const url = import.meta.env.MODE === 'development'
-     *         ? new URL('http://localhost:1234/api')
-     *         : url
-     *       return [url, init]
+     *     (request) => {
+     *       request.headers.set('Authorization', 'Bearer ...')
      *     },
      *   ],
      * }
@@ -86,16 +103,16 @@ interface RetrieveConfig {
      */
     beforeRequestHandlers?: BeforeRequestHandler[];
     /**
-     * Processed if sending the request failed (i.e. the promise returned by `fetch` was rejected). Allows implementing corrective measures.
+     * Run when sending the request failed (i.e. the promise returned by `fetch` was rejected). Allows implementing corrective measures.
      *
      * Exceptions during the processing of a request error handler are not caught.
      *
      * A request error handler can have one of two results:
      *
-     * - maintaining the error state of the request (indicated by returning `{ status: 'maintained', value: error }`)
-     * - correcting the error state of the request (indicated by returning `{ status: 'corrected', value: response }`)
+     * - maintaining the error state of the request
+     * - correcting the error state of the request (indicated by returning a `Response` object)
      *
-     * Returning a result object with the corrected status and a `Response` object allows `retrieve` to continue processing the request as if no error occurred in the first place. Then, no further error request handlers will be processed.
+     * Returning a `Response` object allows `retrieve` to continue processing the request as if no error occurred in the first place. Then, no further error request handlers will be processed.
      *
      * **Example**:
      *
@@ -103,17 +120,15 @@ interface RetrieveConfig {
      * const config = {
      *   url: 'https://api.example.org',
      *   requestErrorHandlers: [
-     *     async (requestError, url, init) => {
+     *     async (error, request) => {
      *       // Do something to fix the error cause
-     *       const response = await fetch(url, init)
-     *
-     *         return { status: 'corrected', value: response }
+     *       return await retrieve(request)
      *     },
      *   ],
      * }
      * ```
      *
-     * Returning a result object with the maintained status and an `Error` object makes `retrieve` continue treating the request as having errored. Note also that all request error handlers will be processed as long as the previous handlers maintain the error state.
+     * Returning an `Error` object (or not returning anything or returning `undefined`) makes `retrieve` continue treating the request as having errored. All request error handlers will be processed as long as the previous handlers maintain the error state (i.e. don't return a `Response` object). A returned `Error` object will be passed to subsequent handlers.
      *
      * **Example**:
      *
@@ -121,11 +136,10 @@ interface RetrieveConfig {
      * const config = {
      *   url: 'https://api.example.org',
      *   requestErrorHandlers: [
-     *     (requestError, url, init) => {
-     *       // Do something with requestError
-     *       requestError.message = 'ERR: ' + requestError.message
-     *
-     *       return { status: 'maintained', value: requestError }
+     *     (error, request) => {
+     *       // Do something with error
+     *       error.message = 'ERR: ' + error.message
+     *       return error
      *     },
      *   ],
      * }
@@ -133,7 +147,7 @@ interface RetrieveConfig {
      */
     requestErrorHandlers?: RequestErrorHandler[];
     /**
-     * Processed if sending the request succeeded and a response with a status code 200–299 was returned (i.e. the promise returned by `fetch` is fulfilled and yields a `Response` object whose `ok` property is set to `true`).
+     * Run when sending the request succeeded and a response with a status code 200–299 was returned (i.e. the promise returned by `fetch` is fulfilled and yields a `Response` object whose `ok` property is set to `true`).
      *
      * Exceptions during the processing of a response success handler are not caught.
      *
@@ -142,8 +156,8 @@ interface RetrieveConfig {
      * ```js
      * const config = {
      *   url: 'https://api.example.org',
-     *   responseErrorHandlers: [
-     *     async (retrieveResponse, url, init) => {
+     *   responseSuccessHandlers: [
+     *     async (retrieveResponse) => {
      *       // Do something with retrieveResponse
      *       return retrieveResponse
      *     },
@@ -153,16 +167,16 @@ interface RetrieveConfig {
      */
     responseSuccessHandlers?: ResponseSuccessHandler[];
     /**
-     * Processed if sending the request succeeded and a response with a status code >=300 was returned (i.e. the promise returned by `fetch` is fulfilled and yields a `Response` object whose `ok` property is set to `false`).
+     * Run when sending the request succeeded and a response with a status code >=300 was returned (i.e. the promise returned by `fetch` is fulfilled and yields a `Response` object whose `ok` property is set to `false`).
      *
      * Exceptions during the processing of a response error handler are not caught.
      *
      * A response error handler can have one of two results:
      *
-     * - maintaining the error state of the response (indicated by returning `{ status: 'maintained', value: error }`)
-     * - correcting the error state of the response (indicated by returning `{ status: 'corrected', value: response }`)
+     * - maintaining the error state of the response
+     * - correcting the error state of the response (indicated by returning a `Response` object)
      *
-     * Returning a result object with the corrected status and a `Response` object allows `retrieve` to continue processing the response as if no error occurred in the first place. Then, no further error response handlers will be processed.
+     * Returning a `Response` object allows `retrieve` to continue processing the response as if no error occurred in the first place. Then, no further error response handlers will be processed.
      *
      * **Example**:
      *
@@ -170,21 +184,17 @@ interface RetrieveConfig {
      * const config = {
      *   url: 'https://api.example.org',
      *   responseErrorHandlers: [
-     *     async (error, retrieveResponse, url, init) => {
-     *       if (retrieveResponse.response.status === 401) {
+     *     async (error, { request, response }) => {
+     *       if (response.status === 401) {
      *         // Do something to fix the error cause (e.g. refresh the user's session)
-     *         const response = await fetch(url, init)
-     *
-     *         return { status: 'corrected', value: response }
+     *         return await retrieve(request)
      *       }
-     *
-     *       return { status: 'maintained', value: error }
      *     },
      *   ],
      * }
      * ```
      *
-     * Returning a result object with the maintained status and an `ResponseError` object makes `retrieve` continue treating the response as having errored. Note also that all response error handlers will be processed as long as the previous handlers maintain the error state.
+     * Returning an `Error` object (or not returning anything or returning `undefined`) makes `retrieve` continue treating the response as having errored. All response error handlers will be processed as long as the previous handlers maintain the error state (i.e. don't return a `Response` object). A returned `Error` object will be passed to subsequent handlers.
      *
      * **Example**:
      *
@@ -192,11 +202,10 @@ interface RetrieveConfig {
      * const config = {
      *   url: 'https://api.example.org',
      *   responseErrorHandlers: [
-     *     async (error, retrieveResponse, url, init) => {
+     *     async (error, retrieveResponse) => {
      *       // Do something with error
      *       error.message = 'ERR: ' + error.message
-     *
-     *       return { status: 'maintained', value: error }
+     *       return error
      *     },
      *   ],
      * }
@@ -204,48 +213,30 @@ interface RetrieveConfig {
      */
     responseErrorHandlers?: ResponseErrorHandler[];
 }
-interface RetrieveResponse {
-    /**
-     * Original `Response` object as returned by `fetch`.
-     */
-    response: Response;
-    /**
-     * Deserialized response body (if applicable).
-     *
-     * The following logic applies to deserialization:
-     *
-     * - Response content type starts with “application/json” or “application/problem+json”: the response body is parsed as JSON (using `Response.prototype.json`).
-     * - For everything else: the response body is parsed as text (using `Response.prototype.text`).
-     */
-    data: unknown;
-}
-interface ErrorCorrectedResult {
-    status: 'corrected';
-    value: Response;
-}
-interface ErrorMaintainedResult<ErrorType> {
-    status: 'maintained';
-    value: ErrorType;
-}
-type ErrorHandlerResult<ErrorType = Error> = ErrorCorrectedResult | ErrorMaintainedResult<ErrorType>;
-type RetrieveFetchParams = [RetrieveConfig['url'], RequestInit];
-type BeforeRequestHandler = (...fetchParams: RetrieveFetchParams) => RetrieveFetchParams | Promise<RetrieveFetchParams>;
-type RequestErrorHandler = (requestError: Error, ...fetchParams: RetrieveFetchParams) => ErrorHandlerResult | Promise<ErrorHandlerResult>;
-type ResponseSuccessHandler = (retrieveResponse: RetrieveResponse) => RetrieveResponse | Promise<RetrieveResponse>;
-type ResponseErrorHandler = (error: Error, retrieveResponse: RetrieveResponse, ...fetchParams: RetrieveFetchParams) => ErrorHandlerResult<Error> | Promise<ErrorHandlerResult<Error>>;
-declare function retrieve(config: RetrieveConfig): Promise<RetrieveResponse>;
+type OptionalPromise<T> = T | Promise<T>;
+type BeforeRequestHandler = (request: Request, init: RequestInit) => OptionalPromise<Response | Request | undefined>;
+type RequestErrorHandler = (error: Error, request: Request, init: RequestInit) => OptionalPromise<Response | Error | undefined>;
+type ResponseSuccessHandler = (retrieveResponse: RetrieveResponse, init: RequestInit) => OptionalPromise<RetrieveResponse | undefined>;
+type ResponseErrorHandler = (error: Error, retrieveResponse: RetrieveResponse, init: RequestInit) => OptionalPromise<RetrieveResponse | Response | Error | undefined>;
+/**
+ * Takes a `RetrieveConfig` or `Request` object and makes a network request using `fetch`.
+ *
+ * When providing a `RetrieveConfig`, several preprocessing steps are performed before creating a `Request` object. That `Request` object (and the `RequestInit` object that was used to create it) is then passed to `config.beforeRequestHandlers` before it's ultimately passed to `fetch`.
+ *
+ * When providing a `Request` object, no preprocessing steps are performed and no interceptors are executed. The `Request` is passed to `fetch` directly. This is primarily intended for retrying requests inside `config.responseErrorHandlers`.
+ */
+declare function retrieve(configOrRequest: RetrieveConfig | Request): Promise<RetrieveResponse>;
 
 declare class ResponseError extends Error {
     name: string;
-    data: RetrieveResponse['data'];
+    request: RetrieveResponse['request'];
     response: RetrieveResponse['response'];
-    constructor(retrieveResponse: RetrieveResponse, message?: string, options?: {
-        cause?: unknown;
-    });
+    data: RetrieveResponse['data'];
+    constructor({ request, response, data }: RetrieveResponse, error?: Error);
     toJSON(): {
         name: string;
         message: string;
     };
 }
 
-export { type BeforeRequestHandler, type ErrorCorrectedResult, type ErrorMaintainedResult, type RequestErrorHandler, ResponseError, type ResponseErrorHandler, type ResponseSuccessHandler, type RetrieveConfig, type RetrieveFetchParams, type RetrieveResponse, retrieve };
+export { type BeforeRequestHandler, type RequestErrorHandler, ResponseError, type ResponseErrorHandler, type ResponseSuccessHandler, type RetrieveConfig, RetrieveResponse, retrieve };
