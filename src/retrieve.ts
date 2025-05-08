71893fc1bc2d394acbab8c3ddc42bfff2d035709
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2025-05-08 19:32:18 +0200

    docs: add some @link annotations in comment

--- src/retrieve.ts
+++ src/retrieve.ts
@@ -242,7 +242,7 @@ const CONTENT_TYPES: Record<BodyType, string> = {
 }
 
 /**
- * Takes a `RetrieveConfig` or `Request` object and makes a network request using `fetch`.
+ * Takes a {@link RetrieveConfig} or {@link Request} object and makes a network request using {@link global.fetch}.
  *
  * When providing a `RetrieveConfig`, several preprocessing steps are performed before creating a `Request` object. That `Request` object (and the `RequestInit` object that was used to create it) is then passed to `config.beforeRequestHandlers` before it's ultimately passed to `fetch`.
  *
